7a8266d0ca866efe0804393c0af5b443c2141401
InfinityWarlock <theo.hintonhallows123@gmail.com>
2025-12-15 21:49:29 +0000

    maybe vercel will magically work it out?

--- views/index.js
+++ views/index.js
@@ -96,9 +96,9 @@ app.get('/api/council/:file', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);

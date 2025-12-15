fd60dd37afceffdd2dd3aa932e461f307fce4ed1
InfinityWarlock <theo.hintonhallows123@gmail.com>
2025-12-15 21:45:19 +0000

    serve index.html correctly?

--- views/index.js
+++ views/index.js
@@ -97,7 +97,7 @@ app.get('/api/council/:file', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('./public/index.html');
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => {

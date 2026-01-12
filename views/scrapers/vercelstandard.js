9217202a6d5c72ba69006debee25e1625ea1a809
InfinityWarlock <theo.hintonhallows123@gmail.com>
2026-01-12 20:52:38 +0000

    council scraper function that stores output in vercel blobs

---
+++ views/scrapers/vercelstandard.js
@@ -0,0 +1,28 @@
+import { collectReformAttendanceData } from './standard';
+import { put } from '@vercel/blob';
+
+export const updateVercelReformAttendanceData = async () => {
+    const jsonCouncils = await fs.readFile('./councils.json');
+    const councils = JSON.parse(jsonCouncils);
+
+    for (let { fileName, councilName, baseUrl } of councils) {
+        try {
+            console.log(`Gathering data for: ${councilName}`);
+            const data = await collectReformAttendanceData(
+                councilName,
+                baseUrl
+            );
+            // this object will eventually contain a list of councillor objects from all parties - for now everyone will be a reform councillor
+            const obj = { councilName, attendanceData: data };
+            const jsonStr = JSON.stringify(obj);
+            const res = await put(`out/${fileName}Data.json`, jsonStr, {
+                access: 'public',
+                allowOverwrite: true,
+            }); //probably dont need the output
+        } catch (err) {
+            console.log(`${councilName} didnt work`);
+            console.error(err);
+        }
+    }
+    return { success: true };
+};

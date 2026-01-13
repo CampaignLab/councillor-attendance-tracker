8e4fcd26ffc376c0f4fe806d7b9f196323bdfb32
InfinityWarlock <theo.hintonhallows123@gmail.com>
2026-01-13 00:28:25 +0000

    serverless syntax?

--- views/index.js
+++ views/index.js
@@ -169,6 +169,7 @@ app.get('/api/updateData', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
+export default app;

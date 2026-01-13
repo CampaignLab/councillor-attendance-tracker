b1f7b9502675b7631da22d9bd9b24482dcf23e5a
InfinityWarlock <theo.hintonhallows123@gmail.com>
2026-01-13 00:43:29 +0000

    maybe didnt need serverless thing

--- views/index.js
+++ views/index.js
@@ -169,7 +169,6 @@ app.get('/api/updateData', async (req, res) => {
     }
 });
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
